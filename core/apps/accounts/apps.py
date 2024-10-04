0d2c0853d10afce5f8e24132bad3f866b1979a07
reza <rezasharafdini973@gmail.com>
2024-10-04 12:06:17 +0330

    Add Accounts App

---
+++ core/apps/accounts/apps.py
@@ -0,0 +1,9 @@
+from django.apps import AppConfig
+from django.utils.translation import gettext_lazy as _
+
+
+class AccountsConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "apps.accounts"
+    verbose_name = _('Account')
+    verbose_name_plural = _('Accounts')
