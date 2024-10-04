8ce7633981be5eb3e8073d712974e314debf9b59
reza <rezasharafdini973@gmail.com>
2024-10-04 16:37:03 +0330

    Add custom User model, admin setup and validation; update requirements

--- core/apps/accounts/admin.py
+++ core/apps/accounts/admin.py
@@ -1,3 +1,66 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth import get_user_model
 
-# Register your models here.
+User = get_user_model()
+
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    list_display = ["id", "phone", "username", "is_active", "is_superuser"]
+    list_filter = ["is_active", "is_superuser"]
+    search_fields = ["phone", "username", "email"]
+    list_editable = ["is_active", "is_superuser"]
+    ordering = ("phone",)
+    readonly_fields = ["created_at", "updated_at"]
+
+    fieldsets = (
+        (
+            "Authentication",
+            {
+                "fields": ("phone", "username", "password"),
+            },
+        ),
+        (
+            "Personal Information",
+            {
+                "fields": ("email", "created_at", "updated_at", "image_file"),
+            },
+        ),
+        (
+            "Permissions",
+            {
+                "fields": ("is_active", "is_superuser"),
+            },
+        ),
+        (
+            "Group Permissions",
+            {
+                "fields": ("groups", "user_permissions"),
+            },
+        ),
+        (
+            "Important Dates",
+            {
+                "fields": ("last_login",),
+            },
+        ),
+    )
+
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": (
+                    "phone",
+                    "username",
+                    "email",
+                    "password1",
+                    "password2",
+                    "is_active",
+                    "is_superuser",
+                ),
+            },
+        ),
+    )
--- core/config/settings.py
+++ core/config/settings.py
@@ -143,3 +143,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 CELERY_BROKER_URL = config("CELERY_BROKER_URL", default="amqp://guest:guest@rabbitmq:5672/")
 CELERY_RESULT_BACKEND = config("CELERY_RESULT_BACKEND", default="redis://redis:6379/0")
 
+
+# accounts model settings
+AUTH_USER_MODEL = "accounts.User"
+
