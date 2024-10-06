20e811641fd69197ae9ace244082787e19412517
reza <rezasharafdini973@gmail.com>
2024-10-06 15:27:30 +0330

    Implement Profile Views; update requirements

--- core/apps/accounts/urls.py
+++ core/apps/accounts/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path, include
+from . import views
 
 app_name = "accounts"
 
 urlpatterns = [
-    path("api/v1/", include("apps.accounts.api.v1.urls"))
-    ]
+    path("api/v1/", include("apps.accounts.api.v1.urls")),
+    path("profile/", views.ProfileView.as_view(), name="profile"),
+]
