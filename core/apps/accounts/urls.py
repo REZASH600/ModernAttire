d92cfb6e1d6b7eda086e8957a7367955fd228728
reza <rezasharafdini973@gmail.com>
2024-10-06 17:28:50 +0330

    Implement Login Views

--- core/apps/accounts/urls.py
+++ core/apps/accounts/urls.py
@@ -6,4 +6,5 @@ app_name = "accounts"
 urlpatterns = [
     path("api/v1/", include("apps.accounts.api.v1.urls")),
     path("profile/", views.ProfileView.as_view(), name="profile"),
+    path("login/", views.LoginView.as_view(), name="login"),
 ]
