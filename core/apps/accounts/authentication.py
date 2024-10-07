eefe6f79a2f895d509ec80eb953767e5bcce2d1a
reza <rezasharafdini973@gmail.com>
2024-10-07 10:22:21 +0330

    Add Email Authentication

---
+++ core/apps/accounts/authentication.py
@@ -0,0 +1,19 @@
+from django.contrib.auth import get_user_model
+from django.contrib.auth.backends import BaseBackend
+
+User = get_user_model()
+
+class EmailAuthentication(BaseBackend):
+    def authenticate(self, request, username, password=None):
+        try:
+            user = User.objects.get(email=username)
+            if user and user.check_password(password):
+                return user
+            return None
+        except User.DoesNotExist:
+            return None
+    def get_user(self, user_id):
+        try:
+            return User.objects.get(id=user_id)
+        except User.DoesNotExist:
+            return None
