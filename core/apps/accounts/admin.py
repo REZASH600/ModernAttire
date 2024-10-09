ae6e8e207133fe742756a7d8a17ce66ff07cdbc1
reza <rezasharafdini973@gmail.com>
2024-10-09 22:41:07 +0330

    Add Otp model to Accounts App

--- core/apps/accounts/admin.py
+++ core/apps/accounts/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth import get_user_model
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
-
+from . import models
 User = get_user_model()
 
 
@@ -65,3 +65,6 @@ class UserAdmin(BaseUserAdmin):
             },
         ),
     )
+
+
+admin.site.register(models.Otp)
