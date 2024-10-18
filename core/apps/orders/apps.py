9e15698504eb452218805779a839dd1d37539b86
reza <rezasharafdini973@gmail.com>
2024-10-18 16:54:42 +0330

    Add Orders App

---
+++ core/apps/orders/apps.py
@@ -0,0 +1,9 @@
+from django.apps import AppConfig
+from django.utils.translation import gettext_lazy as _
+
+
+class OrdersConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "apps.orders"
+    verbose_name = _("Order")
+    verbose_name_plural = _("Orders")
